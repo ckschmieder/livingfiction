878dac87917dddabcfba0178c2325993f79c944b
Chris Schmieder <ckschmieder@gmail.com>
2015-12-13 18:52:16 -0500

    Implementation of flexbox to the navbar looks complete. It's been a while since a commit so there are likely other minor style changes. Also, added folder named invisible to ignore file to store some outdated components.

--- php/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-	<a href="index.html">Home</a> | <a href="ourteam.html">Our Team</a> | <a href="testimonials.html">Testimonials</a> | <a href="projects.html">Projects</a> | <a href="contact.html">Contact</a><br/><br/>
-	<a href="images.html">Images</a> |  <a href="http://www.statenweb.com">Web Design</a>  | Website by YouAreMopped
-</footer>
